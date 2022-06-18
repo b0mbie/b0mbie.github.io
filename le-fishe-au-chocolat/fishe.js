29a959e3f3e6ca4afb9c4d5eb7b4135e85d1e3eb
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2022-06-18 20:18:18 +0300

    Push working fishe

---
+++ le-fishe-au-chocolat/fishe.js
@@ -0,0 +1,78 @@
+let caption = null,
+    fishe = null,
+    startBtn = null;
+
+let intro = null;
+let audioID = "wgNPCKuX9ds";
+let player = null;
+
+/// Initialize stuff.
+function init()
+{
+    // get elements
+    caption = document.getElementById("caption");
+    fishe = document.getElementById("fishe");
+    startBtn = document.getElementById("startbtn");
+    startBtn.addEventListener("click", () => {
+        start();
+        startBtn.remove();
+    });
+
+    // create intro audio
+    intro = document.createElement("audio");
+    intro.setAttribute("src", "./intro.wav");
+    intro.setAttribute("preload", "auto");
+    intro.setAttribute("controls", "none");
+    intro.style.display = "none";
+    document.body.appendChild(intro);
+
+    // stretch caption
+    stretchText();
+}
+
+/// Stretch caption width to fit into the same size as fishe.
+function stretchText()
+{
+    let cwidth = caption.getBoundingClientRect().width / (caption.scaleFactor || 1);
+    let fwidth = fishe.getBoundingClientRect().width;
+    let factor = fwidth / cwidth;
+    if (factor < 1.02 && factor > 0.98) return;
+    caption.style.transform = "scaleX(" + factor + ')';
+    caption.scaleFactor = factor;
+}
+
+/// Play "Happy Day in Paris".
+function playMusic()
+{
+    player.playVideo();
+}
+
+/// Play intro, then music.
+function start()
+{
+    intro.play();
+    intro.addEventListener("ended", playMusic);
+}
+
+function onYouTubeIframeAPIReady()
+{
+    let div = document.createElement("div");
+    div.setAttribute("id", "youtube-player");
+    document.body.appendChild(div);
+
+    player = new YT.Player("youtube-player", {
+        height: '0',
+        width: '0',
+        videoId: audioID,
+        playerVars: {
+            autoplay: '0',
+            loop: '1'
+        },
+        events: {
+            'onReady': function (e)
+            {
+                console.log("YouTube player ready\nLet's go out\nto have some\nfish for dinner");
+            }
+        }
+    });
+}
